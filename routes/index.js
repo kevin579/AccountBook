752fc588d1c0b041541088cc1f4c882beb1e9a33
kevin579 <125067771+kevin579@users.noreply.github.com>
2025-07-16 21:54:51 -0400

    structure change

---
+++ routes/index.js
@@ -0,0 +1,130 @@
+const express = require('express');
+const router = express.Router();
+
+const shortid = require('shortid')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync(__dirname+'/../data/db.json')
+
+const moment = require('moment')
+const db = low(adapter)
+
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+
+const model = require('../database/AccountModel.js')
+const config = require('../database/config.js')
+
+function checkLogin(req,res,next){
+  if (!req.session.uname){
+    return res.redirect('/users//login')
+  }
+  const token = req.cookies.token
+  jwt.verify(token,config.secret,(err,data)=>{
+    if (err||!data){
+      return res.redirect('/users//login')
+    } 
+    if (data){
+      req.user = data;
+    }
+
+  })
+  
+  next();
+}
+
+/* Main page of account book. */
+router.get('/account', checkLogin,function(req, res, next) {
+
+  // let accounts = db.get('accounts').value();
+  console.log(req.user)
+  model.find().sort({time:-1})
+  .then(data=>{
+    console.log(data)
+    // res.json({
+    //   code:'0000',
+    //   msg:'',
+    //   data:data
+    // })
+    res.render('account',{accounts:data, moment:moment})
+  })
+  .catch(err=>{
+    // res.status(500).send('reading error')
+    res.json({
+      code:'1001',
+      msg:'error',
+      data:null
+    })
+  })
+  
+});
+
+router.get('/account/create', checkLogin,function(req, res, next) {
+  res.render('create', { title: 'Express' });
+});
+
+router.post('/account',(req,res)=>{
+  // let id = shortid.generate();
+  // db.get('accounts').push({id:id, ...req.body}).write();
+
+  model.create({
+    ...req.body,
+    date: moment(req.body.date).toDate()
+  }).then((err,data)=>{
+    if (err){
+      console.log(err);
+      return;
+    }
+    res.json({
+      code:'0000',
+      msg:'',
+      data:data
+    })
+  })
+  res.redirect('/account');
+})
+
+//deleting account
+
+router.get('/account/:id', checkLogin, (req,res)=>{
+    let id = req.params.id;
+    // db.get('accounts').remove({id:id}).write();
+    model.deleteOne({_id:id}).then(
+      data=>{
+        console.log(data);
+        res.redirect('/account')
+    }
+    ).catch(
+      err=>{
+        console.log(err);
+        res.status(500).send('error')
+      }
+    )
+});
+
+
+// query an account
+// router.get('/account/:id',(req,res)=>{
+//   let {id} = req.params;
+//   model.findById(id)
+//     .then(
+//       data=>{
+//         res.json({
+//           code:'0000',
+//           msg:'',
+//           data:data
+//         })
+//       }
+//     ).catch(
+//       err=>{
+//         console.log(err);
+//         res.json({
+//           code:'1004',
+//           msg:'Cannot load data',
+//           data:null
+//         })
+//       }
+//     ) 
+// })
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var router = express.Router();
+const shortid = require('shortid')
+const md5 = require('md5')
+const session = require('express-session')
+const path = require('path')
+const jwt = require('jsonwebtoken')
+
+const model = require('../database/UserModel')
+const config = require('../database/config.js')
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/reg',(req,res)=>{
+  res.render('register')
+})
+
+router.post('/reg',(req,res)=>{
+  console.log(req.body.password);
+  model.create({...req.body,password:md5(req.body.password)}).then(
+    res.json(req.body)
+
+  ).catch(
+    err =>{
+      if (err){
+        console.log(err);
+        res.status(500).send('error')
+      }
+    }
+  )
+})
+
+router.get('/login',(req,res)=>{
+  res.render('login')
+})
+
+router.post('/login',(req,res)=>{
+  const {uname,password} = req.body;
+  model.findOne({uname:uname,password:md5(password)}).then(
+    data =>{
+      console.log(password);
+      if (data){
+        req.session.uname=data.uname;
+        req.session._id=data._id;
+        let token = jwt.sign({
+          uname:uname,
+          _id: data._id
+        },config.secret,{
+          expiresIn:60*60*24*3
+        })
+        res.cookie('token', token, {
+          httpOnly: true, 
+          maxAge: 1000 * 60 * 60 * 24 * 3
+        });
+        res.redirect('/account')
+      }else{
+        res.send('no such user')
+      }
+    }
+  ).catch(
+    err=>{
+      if(err){
+        console.log(err);
+        res.status(500).send('failed');
+      }
+    }
+  )
+})
+
+router.get('/logout',(req,res)=>{
+  req.session.destroy(()=>{
+    res.redirect('/users/login');
+  })
+})
+module.exports = router;
